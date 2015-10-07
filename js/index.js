36e5cc86ac11f9f6850a18cb77f5dc6597d03852
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2015-10-07 10:18:44 -0700

    Finishes out path animations.

--- js/index.js
+++ js/index.js
@@ -42,7 +42,6 @@ $(".travel-hover").hover(function() {
   function delayStroke(path) {
     return function() {
       path.style.strokeDashoffset = '0';
-      console.log(path);
     }
   }
 
@@ -68,6 +67,88 @@ $(".travel-hover").hover(function() {
     // Define our transition
     path.style.transition = path.style.WebkitTransition =
       'stroke-dashoffset ' + time.toString() + 'ms ease-in-out';
+
+    // Go! (eventually)
+    setTimeout(delayStroke(path), timeElapsed);
+    timeElapsed += time;
+  }
+}, function(){});
+
+$(".hackathons-hover").hover(function() {
+  var hackathonPaths = ["rim", "cup", "handle1", "handle2", "base"];
+  var timeElapsed = 0;
+  var totalAnimation = 2000;
+  var totalLength = 0;
+
+  function delayStroke(path) {
+    return function() {
+      path.style.strokeDashoffset = '0';
+    }
+  }
+
+  for (i = 0; i < hackathonPaths.length; i++) {
+    totalLength += document.querySelector('.hackathons #' + hackathonPaths[i]).getTotalLength();
+  }
+
+  for (i = 0; i < hackathonPaths.length; i++) {
+    var path = document.querySelector('.hackathons #' + hackathonPaths[i]);
+
+    var pathLength = path.getTotalLength();
+    var time = (pathLength / totalLength) * totalAnimation;
+
+    // Clear any previous transition
+    path.style.transition = path.style.WebkitTransition =
+      'none';
+    // Set up the starting positions
+    path.style.strokeDasharray = pathLength + ' ' + pathLength;
+    path.style.strokeDashoffset = pathLength;
+    // Trigger a layout so styles are calculated & the browser
+    // picks up the starting position before animating
+    path.getBoundingClientRect();
+    // Define our transition
+    path.style.transition = path.style.WebkitTransition =
+      'stroke-dashoffset ' + time.toString() + 'ms linear';
+
+    // Go! (eventually)
+    setTimeout(delayStroke(path), timeElapsed);
+    timeElapsed += time;
+  }
+}, function(){});
+
+$(".plants-hover").hover(function() {
+  var plantsPaths = ["core", "center", "petal12", "petal1", "petal2", "petal3", "petal4", "petal5", "petal6", "petal7", "petal8", "petal9", "petal10", "petal11"];
+  var timeElapsed = 0;
+  var totalAnimation = 2000;
+  var totalLength = 0;
+
+  function delayStroke(path) {
+    return function() {
+      path.style.strokeDashoffset = '0';
+    }
+  }
+
+  for (i = 0; i < plantsPaths.length; i++) {
+    totalLength += document.querySelector('.plants #' + plantsPaths[i]).getTotalLength();
+  }
+
+  for (i = 0; i < plantsPaths.length; i++) {
+    var path = document.querySelector('.plants #' + plantsPaths[i]);
+
+    var pathLength = path.getTotalLength();
+    var time = (pathLength / totalLength) * totalAnimation;
+
+    // Clear any previous transition
+    path.style.transition = path.style.WebkitTransition =
+      'none';
+    // Set up the starting positions
+    path.style.strokeDasharray = pathLength + ' ' + pathLength;
+    path.style.strokeDashoffset = pathLength;
+    // Trigger a layout so styles are calculated & the browser
+    // picks up the starting position before animating
+    path.getBoundingClientRect();
+    // Define our transition
+    path.style.transition = path.style.WebkitTransition =
+      'stroke-dashoffset ' + time.toString() + 'ms linear';
 
     // Go! (eventually)
     setTimeout(delayStroke(path), timeElapsed);
