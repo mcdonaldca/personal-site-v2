2894bc9f004d4bca2aac5b1d1aa374ac53a81078
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2015-10-07 09:08:37 -0700

    Adds preliminary SVG path drawing animation.

--- js/index.js
+++ js/index.js
@@ -33,25 +33,44 @@ $('svg.cam').click(function() {
 
 
 
-
-
-var cloudPath = document.querySelector('.travel #mountain-front');
-
-var cloudLength = cloudPath.getTotalLength();
-// Clear any previous transition
-cloudPath.style.transition = cloudPath.style.WebkitTransition =
-  'none';
-// Set up the starting positions
-cloudPath.style.strokeDasharray = cloudLength + ' ' + cloudLength;
-cloudPath.style.strokeDashoffset = cloudLength;
-// Trigger a layout so styles are calculated & the browser
-// picks up the starting position before animating
-cloudPath.getBoundingClientRect();
-// Define our transition
-cloudPath.style.transition = cloudPath.style.WebkitTransition =
-  'stroke-dashoffset 2s linear';
-// Go!
-
 $(".travel-hover").hover(function() {
-  cloudPath.style.strokeDashoffset = '0';
+  var travelPaths = ["cloud", "bird1", "bird2", "bird3", "mountain-front", "sun", "sunbeam1", "sunbeam2", "sunbeam3", "mountain-back"];
+  var timeElapsed = 0;
+  var totalAnimation = 2000;
+  var totalLength = 0;
+
+  function delayStroke(path) {
+    return function() {
+      path.style.strokeDashoffset = '0';
+      console.log(path);
+    }
+  }
+
+  for (i = 0; i < travelPaths.length; i++) {
+    totalLength += document.querySelector('.travel #' + travelPaths[i]).getTotalLength();
+  }
+
+  for (i = 0; i < travelPaths.length; i++) {
+    var path = document.querySelector('.travel #' + travelPaths[i]);
+
+    var pathLength = path.getTotalLength();
+    var time = (pathLength / totalLength) * totalAnimation;
+
+    // Clear any previous transition
+    path.style.transition = path.style.WebkitTransition =
+      'none';
+    // Set up the starting positions
+    path.style.strokeDasharray = pathLength + ' ' + pathLength;
+    path.style.strokeDashoffset = pathLength;
+    // Trigger a layout so styles are calculated & the browser
+    // picks up the starting position before animating
+    path.getBoundingClientRect();
+    // Define our transition
+    path.style.transition = path.style.WebkitTransition =
+      'stroke-dashoffset ' + time.toString() + 'ms ease-in-out';
+
+    // Go! (eventually)
+    setTimeout(delayStroke(path), timeElapsed);
+    timeElapsed += time;
+  }
 }, function(){});
